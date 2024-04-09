8eef8d4f547ff02172817f76e91e3ef480559d7d
Khang Nguyễn Vĩ <vikhang17112002@gmail.com>
2024-04-09 08:25:54 +0700

    create token and register account

--- src/main/java/vn/edu/hcmuaf/api_clothes_ecommerce_shop/Config/SecurityConfig.java
+++ src/main/java/vn/edu/hcmuaf/api_clothes_ecommerce_shop/Config/SecurityConfig.java
@@ -1,16 +1,21 @@
 package vn.edu.hcmuaf.api_clothes_ecommerce_shop.Config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import vn.edu.hcmuaf.api_clothes_ecommerce_shop.Service.AuthService;
 
 @Configuration
 @EnableWebSecurity
@@ -18,38 +23,67 @@ import org.springframework.web.cors.CorsConfiguration;
 public class SecurityConfig {
     private UserDetailsService userDetailsService;
 
-    public SecurityConfig(UserDetailsService userDetailsService) {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+
+    @Autowired
+    public SecurityConfig(UserDetailsService userDetailsService,
+                          JwtAuthenticationFilter jwtAuthFilter,
+                          AuthenticationProvider authenticationProvider) {
         this.userDetailsService = userDetailsService;
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.authenticationProvider = authenticationProvider;
     }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+//
 //    @Bean
-//    public DaoAuthenticationProvider authenticationProvider(AuthDao authDao) {
+//    public DaoAuthenticationProvider authenticationProvider(AuthService authService) {
 //        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-//        auth.setUserDetailsService(authDao);
+//        auth.setUserDetailsService(authService);
 //        auth.setPasswordEncoder(passwordEncoder());
 //        return auth;
 //    }
 
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests(auth ->
+//                auth
+//                        // auth
+//                        .requestMatchers(HttpMethod.POST, "api/v1/auth/login").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "api/v1/auth/register").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "api/v1/auth/register/confirm").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "api/v1/auth/forgot-password").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "api/v1/auth/forgot-password/confirm").permitAll()
+//                        .anyRequest().permitAll()
+//        );
+//        http.httpBasic();
+//        http.csrf().disable();
+//        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+//        return http.build();
+//    }
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(auth ->
-                auth
-                        // auth
-                        .requestMatchers(HttpMethod.POST, "api/v1/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "api/v1/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "api/v1/auth/register/confirm").permitAll()
-                        .requestMatchers(HttpMethod.POST, "api/v1/auth/forgot-password").permitAll()
-                        .requestMatchers(HttpMethod.POST, "api/v1/auth/forgot-password/confirm").permitAll()
-                        .anyRequest().permitAll()
-        );
-        http.httpBasic();
-        http.csrf().disable();
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
